cb80458823d44454722923ef4af6ae0889b3db39
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-12-14 11:00:21 +0300

    Add some element in the json file and use a specific attribute to make it work as an html element

--- pages/LyricsSong.js
+++ pages/LyricsSong.js
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { Context } from '../Context';
 
 function LyricsSong() {
     const {allSongs} = useContext(Context)
     const {songId} = useParams();
+    let history = useHistory();
 
     const songLyrics = allSongs.find(song => song.id === Number(songId));
 
     return (
         <section>
+            <i onClick={() => history.goBack()} className="ri-arrow-left-line"></i>
             <h2>{songLyrics.name}: {songLyrics.title}</h2>
             <div className="song-list">
                 <h3>Lyrics</h3>
-                <p className="lyrics">{songLyrics.lyrics}</p>
+                <div dangerouslySetInnerHTML={{ __html: songLyrics.lyrics }}></div>
             </div>
 
         </section>
